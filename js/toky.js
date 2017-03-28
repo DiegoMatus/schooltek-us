36bc21ec6dd0d20a13baa86b9eb99666683bd459
Alexander <alexhernandez840@gmail.com>
2017-03-28 12:51:35 -0600

    feat - se agrego script de llamadas

---
+++ js/toky.js
@@ -0,0 +1,6 @@
+(function(v,p){
+    var s=document.createElement('script');
+    s.src='https://app.toky.co/resources/widgets/toky-widget.js?v='+v;
+    s.onload=function(){Toky.load(p);};
+    document.head.appendChild(s);
+    })('59e5299', {"$lang":"es","$username":"Schooltek","$bubble":false,"$position":"left","$text":"Llámanos Gratis"})

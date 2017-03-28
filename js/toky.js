1ebf8c9db265915a582bdff8f14bcc5ab4a6f389
Alexander <alexhernandez840@gmail.com>
2017-03-28 14:15:10 -0600

    fix - se modifico script de llamadas

--- js/toky.js
+++ js/toky.js
@@ -1,6 +1,6 @@
 (function(v,p){
-    var s=document.createElement('script');
-    s.src='https://app.toky.co/resources/widgets/toky-widget.js?v='+v;
-    s.onload=function(){Toky.load(p);};
-    document.head.appendChild(s);
-    })('59e5299', {"$lang":"es","$username":"Schooltek","$bubble":false,"$position":"left","$text":"Llámanos Gratis"})
+var s=document.createElement('script');
+s.src='https://app.toky.co/resources/widgets/toky-widget.js?v='+v;
+s.onload=function(){Toky.load(p);};
+document.head.appendChild(s);
+})('59e8beb', {"$lang":"en","$username":"Schooltek","$bubble":false,"$position":"left","$text":"Llámanos Gratis","$color":"#6cb670,#70c080,#ffffff"})

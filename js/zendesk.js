bc9b151a19a86a333ee4edb1ce3b3b0329493b9f
Alexander <alexhernandez840@gmail.com>
2017-04-07 23:32:59 -0500

    feat - agregando chat y cambio de leguaje del mismo

---
+++ js/zendesk.js
@@ -0,0 +1,9 @@
+window.$zopim||(function(d,s){var z=$zopim=function(c){
+z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='https://v2.zopim.com/?4g4cAA5l5XN2gYFEdsbRjY5TfqcBpIPd';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+$zopim(function() {
+    $zopim.livechat.setLanguage('en');
+  });

2aaa34aeb7fde15c31288e48c0332901176a94a9
Alexander <alexhernandez840@gmail.com>
2017-04-10 11:25:43 -0500

    refactory - se cambio color de toky

--- js/toky.js
+++ js/toky.js
@@ -3,4 +3,4 @@ var s=document.createElement('script');
 s.src='https://app.toky.co/resources/widgets/toky-widget.js?v='+v;
 s.onload=function(){Toky.load(p);};
 document.head.appendChild(s);
-})('59e8beb', {"$lang":"en","$username":"Schooltek","$bubble":false,"$position":"left","$text":"Llámanos Gratis","$color":"#6cb670,#70c080,#ffffff"})
+})('59e8beb', {"$lang":"en","$username":"Schooltek","$bubble":false,"$position":"left","$text":"Llámanos Gratis","$color":"#BE3037,#BE3037,#ffffff"})
